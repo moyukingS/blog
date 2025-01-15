847304afaf22974fa410ae0fc5da91c9faa6f971
moyu wang <95458343+moyukingS@users.noreply.github.com>
2025-01-16 00:00:17 +0800

    feat✨: Enhance Header component with logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,8 @@
 'use client';
 import Link from 'next/link';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import Logo from '@/public/logo.png';
 
 export default function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -16,8 +18,11 @@ export default function Header() {
 
   return (
     <header
-      className={`fixed flex w-full items-center justify-end px-7 ${isScrolled ? 'pt-4' : 'pt-7'} transition-all duration-500`}
+      className={`fixed flex w-full items-center justify-between px-7 ${isScrolled ? 'pt-4' : 'pt-7'} transition-all duration-500`}
     >
+      <Link href="/">
+        <Image width={60} height={60} src={Logo} alt="logo" />
+      </Link>
       <nav className="rounded-full bg-white px-8 py-4">
         <ul className="flex items-center justify-evenly gap-6">
           <li className="cursor-pointer text-2xl duration-500 hover:text-anon-600">
