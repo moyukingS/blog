65fa09517459cf8e4838a8b20dce96e5b7ab7f06
moyu wang <95458343+moyukingS@users.noreply.github.com>
2025-01-18 23:02:35 +0800

    fix🐛: Update Card component rendering in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
       <div className="container mt-4 grid grid-cols-4 gap-10">
         {Array.from({ length: 10 }).map((item, index) => {
-          return <Card key={index}>{item}</Card>;
+          return <Card key={index}></Card>;
         })}
       </div>
     </main>
